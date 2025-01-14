a9ba4b03e4bf954022aa86c3f38250e9b1491d36
VKasurinen <vaino.kasurinen@hotmail.fi>
2025-01-14 18:05:19 +0200

    HomeScreen update

--- app/src/main/java/com/vkasurinen/vknews/presentation/homescreen/components/TopNews.kt
+++ app/src/main/java/com/vkasurinen/vknews/presentation/homescreen/components/TopNews.kt
@@ -1,5 +1,7 @@
 package com.vkasurinen.vknews.presentation.homescreen.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -7,6 +9,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -15,7 +18,10 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.vkasurinen.vknews.domain.model.Article
 import com.vkasurinen.vknews.util.Screen
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TopNews(
     topHeadlines: List<Article>,
@@ -41,12 +47,43 @@ fun TopNews(
                             .padding(vertical = 8.dp),
                         verticalArrangement = Arrangement.Center
                     ) {
-                        Text(
-                            text = "Breaking",
-                            fontSize = 18.sp,
-                            fontWeight = FontWeight.SemiBold,
-                            color = MaterialTheme.colorScheme.primary
-                        )
+
+                        Row(
+                            verticalAlignment = Alignment.Bottom,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ){
+                            Text(
+                                text = "Breaking",
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.SemiBold,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+
+                            Text(
+                                text = article.source.name,
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.Medium,
+                                color = MaterialTheme.colorScheme.onBackground,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+
+                            val parsedDate = ZonedDateTime.parse(article.publishedAt)
+                            val formattedDate = parsedDate.format(DateTimeFormatter.ofPattern("dd:MM:yyyy"))
+
+                            Text(
+                                text = formattedDate,
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.Medium,
+                                color = MaterialTheme.colorScheme.onBackground,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+
+
+
+                        }
+
                         Spacer(modifier = Modifier.height(5.dp))
                         Text(
                             text = article.title,
